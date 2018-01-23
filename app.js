49215bea414c709ba6e03ae6e50eddbb8643c3c1
danielcortes <dcortes@velocitypartners.net>
2018-01-23 17:32:21 -0500

    creating log

---
+++ app.js
@@ -0,0 +1,35 @@
+const yargs = require('yargs');
+const arg = yargs.argv._[0];
+
+const { createLogger, transports, format, format: { combine, timestamp, prettyPrint, printf } } = require('winston');
+
+const today = new Date()
+let month = today.getMonth() + 1;
+month = month < 10 ? '0' + month : month;
+const fileDate = `${today.getFullYear()}-${month}-${today.getDate()}`
+
+const myFormat = printf(info => {
+    return `${info.timestamp} - [${info.level}]: ${info.message}`;
+});
+
+const logger = createLogger({
+    format: combine(
+        timestamp(),
+        myFormat
+    ),
+    transports: [
+        new transports.File({ filename: `${fileDate}.log` }),
+    ]
+})
+
+switch (arg) {
+    case 'add':
+        logger.info('Is adding info')
+        break;
+    case 'remove':
+        logger.info('Is removing info')
+        break;
+    default:
+        logger.error('An error has occur')
+        break;
+}
