7891842d191eb1a2c4de6f47c70e629c9d04c31f
danielcortes <dcortes@velocitypartners.net>
2018-02-07 16:36:36 -0500

    Moving to github and adding eslint

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 const yargs = require('yargs');
+
 const arg = yargs.argv._[0];
 
-const { createLogger, transports, format, format: { combine, timestamp, prettyPrint, printf } } = require('winston');
+const {
+    createLogger, transports, format, format: {
+        combine, timestamp, prettyPrint, printf,
+    },
+} = require('winston');
 
-const today = new Date()
+const today = new Date();
 let month = today.getMonth() + 1;
-month = month < 10 ? '0' + month : month;
+month = month < 10 ? `0${month}` : month;
 const fileDate = `${today.getFullYear()}-${month}-${today.getDate()}`;
 
-const myFormat = printf(info => {
-    return `${info.timestamp} - [${info.level}]: ${info.message}`;
-});
+const myFormat = printf(info => `${info.timestamp} - [${info.level}]: ${info.message}`);
 
 const logger = createLogger({
     format: combine(
         timestamp(),
-        myFormat
+        myFormat,
     ),
     transports: [
-        new transports.File({ filename: `${fileDate}.log` })
-    ]
-})
+        new transports.File({ filename: `${fileDate}.log` }),
+    ],
+});
 
 switch (arg) {
-    case 'add':
-        logger.info('Is adding info')
-        break;
-    case 'remove':
-        logger.info('Is removing info')
-        break;
-    default:
-        logger.error('An error has occur')
-        break;
+case 'add':
+    logger.info('Is adding info');
+    break;
+case 'remove':
+    logger.info('Is removing info');
+    break;
+default:
+    logger.error('An error has occur');
+    break;
 }
